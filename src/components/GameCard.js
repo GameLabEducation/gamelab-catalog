703ec4ff887af4afd0700c73d2de94607a2786f1
Emiliano <e.acevedo.felip@gmail.com>
2023-03-31 13:41:38 -0300

    Proper alt att in GameCard's Image

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -10,7 +10,7 @@ export default function ({ title, description, imageUrl, gameSiteUrl }) {
 
   return (
     <div className='p-10 rounded-3xl max-w-[510px] flex flex-col' style={cardContainerStyles}>
-      <Image src={imageUrl} alt='Sodapop factory screen' className='mb-4 h-[240px] w-[426px]' width={426} height={240} />
+      <Image src={imageUrl} className='mb-4 h-[240px] w-[426px]' width={426} height={240} alt={title + ' preview image'} />
       <h2 className='text-4xl text-center mb-6'>{title}</h2>
       <p className='text-center mb-5'>{description}</p>
       <a className='mt-auto p-4 bg-blue-600 hover:bg-blue-900 transition-all text-white font-bold rounded-lg block mx-auto w-[200px] text-center uppercase text-sm' href={gameSiteUrl} target='_blank' rel='noreferrer'>{t('goToSimulator')}</a>

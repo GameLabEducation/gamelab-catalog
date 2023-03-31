c8ef72667c50781ee489bd49f34f6287ab4bd892
Emiliano <e.acevedo.felip@gmail.com>
2023-03-31 14:12:04 -0300

    responsive header and url for contact anchor

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,14 +9,18 @@ const headerStyles = {
 export default function ({ setSearchString }) {
   const { toogleLang, lang, t } = useTranslation()
 
+  const helpUrl = lang === 'es'
+    ? 'https://gamelabeducation.com/contact/'
+    : 'https://gamelabeducation.com/en/contact/'
+
   return (
     <header className='flex items-center px-20 py-8' style={headerStyles}>
       <a href='https://gamelabeducation.com/'>
         <Image src='/images/gamelab-header-logo.png' width='100' height='100' alt='Gamelab logo' />
       </a>
-      <div className='ml-auto flex flex-col justify-end gap-1'>
+      <div className='ml-auto  flex-col justify-end gap-1 hidden sm:flex'>
         <div className='flex justify-between items-center'>
-          <a href='https://gamelabeducation.com/en/contact/' className='text-end text-white hover:underline uppercase font-bold' target='_blank' rel='noreferrer'>{t('contactUs', 'mama')}</a>
+          <a href={helpUrl} className='text-end text-white hover:underline uppercase font-bold' target='_blank' rel='noreferrer'>{t('contactUs', 'mama')}</a>
           <button className='p-2' onClick={toogleLang}>{lang === 'es' ? '🇺🇸' : '🇪🇸'}</button>
         </div>
         <SearchInput setSearchString={setSearchString} />

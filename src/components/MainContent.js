27b27bee212e3e332ed5f8034363d98af66e6a07
Emiliano <e.acevedo.felip@gmail.com>
2023-03-31 14:11:13 -0300

    Padding for title page and game urls

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -4,20 +4,22 @@ import HelpCard from './HelpCard'
 
 const games = [
   { title: 'Sodapop Game', transKey: 'sodapopDescription', gameSiteUrl: 'https://sp-beta.gamelab.cl', imageUrl: '/images/sodapop.png' },
-  { title: 'Winegame  ', transKey: 'wineGameDescription', gameSiteUrl: 'https://sp-beta.gamelab.cl', imageUrl: '/images/winegame.png' },
-  { title: 'Startup Journey Game', transKey: 'sjgDescription', gameSiteUrl: 'https://sp-beta.gamelab.cl', imageUrl: '/images/sjg.png' },
-  { title: 'Warehouse Game', transKey: 'warehouseDescription', gameSiteUrl: 'https://sp-beta.gamelab.cl', imageUrl: '/images/warehouse.png' },
-  { title: 'Negotiation Game', transKey: 'negotiationDescription', gameSiteUrl: 'https://sp-beta.gamelab.cl', imageUrl: '/images/negotiation.png' },
-  { title: 'Pricingame', transKey: 'pricinDescription', gameSiteUrl: 'https://sp-beta.gamelab.cl', imageUrl: '/images/pricin.png' },
-  { title: 'Balanced Scorecard Game', transKey: 'balancedDescription', gameSiteUrl: 'https://sp-beta.gamelab.cl', imageUrl: '/images/balanced.png' }
+  { title: 'Winegame  ', transKey: 'wineGameDescription', gameSiteUrl: 'https://wine-beta.gamelab.cl', imageUrl: '/images/winegame.png' },
+  { title: 'Startup Journey Game', transKey: 'sjgDescription', gameSiteUrl: 'https://sjg-beta.gamelab.cl', imageUrl: '/images/sjg.png' },
+  { title: 'Warehouse Game', transKey: 'warehouseDescription', gameSiteUrl: 'https://wg-beta.gamelab.cl', imageUrl: '/images/warehouse.png' },
+  { title: 'Negotiation Game', transKey: 'negotiationDescription', gameSiteUrl: 'https://ng-beta.gamelab.cl', imageUrl: '/images/negotiation.png' },
+  { title: 'Pricingame', transKey: 'pricinDescription', gameSiteUrl: 'https://pg-beta.gamelab.cl', imageUrl: '/images/pricin.png' },
+  { title: 'Balanced Scorecard Game', transKey: 'balancedDescription', gameSiteUrl: 'https://bsc-beta.gamelab.cl', imageUrl: '/images/balanced.png' }
 ]
 
 export default function ({ searchString }) {
   const { t } = useTranslation()
   return (
     <div className='my-20'>
-      <h1 className='text-center text-4xl mb-2'>{t('productCatalogue')}</h1>
-      <p className='text-center text-xl mb-8'>{t('clickToAccess')}</p>
+      <div className='px-4'>
+        <h1 className='text-center text-4xl mb-2'>{t('productCatalogue')}</h1>
+        <p className='text-center text-xl mb-8'>{t('clickToAccess')}</p>
+      </div>
       <div className='flex justify-center flex-wrap gap-20 px-3'>
         {
           games

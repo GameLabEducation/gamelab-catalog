34910f5603d5abaa77f2da56dbf8d086fea3b302
Emiliano <e.acevedo.felip@gmail.com>
2023-03-31 13:41:06 -0300

    Header gamelag logo href to gamelabeducation we

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,8 +10,10 @@ export default function ({ setSearchString }) {
   const { toogleLang, lang, t } = useTranslation()
 
   return (
-    <header className='h-[112px] flex items-center px-10' style={headerStyles}>
-      <Image src='/images/gamelab-header-logo.png' width='100' height='100' />
+    <header className='flex items-center px-20 py-8' style={headerStyles}>
+      <a href='https://gamelabeducation.com/'>
+        <Image src='/images/gamelab-header-logo.png' width='100' height='100' alt='Gamelab logo' />
+      </a>
       <div className='ml-auto flex flex-col justify-end gap-1'>
         <div className='flex justify-between items-center'>
           <a href='https://gamelabeducation.com/en/contact/' className='text-end text-white hover:underline uppercase font-bold' target='_blank' rel='noreferrer'>{t('contactUs', 'mama')}</a>

2086ed0cbeacad4bdf626fac2e3675948ead4b7e
Emiliano <e.acevedo.felip@gmail.com>
2023-03-31 13:41:54 -0300

    Remove reactive title

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,14 @@ import Header from '@/components/Header'
 import MainContent from '@/components/MainContent'
 import Head from 'next/head'
 import Footer from '@/components/Footer'
-import { useTranslation } from '@/hooks/useTranslations'
 
 export default function Home () {
   const [searchString, setSearchString] = useState('')
-  const { t } = useTranslation()
 
   return (
     <>
       <Head>
-        <title>Gamelab Education - {t('catalog')}</title>
+        <title>Gamelab Education catalog</title>
         <meta name='description' content='Games and Simulators catalog by Gamelab Education' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />

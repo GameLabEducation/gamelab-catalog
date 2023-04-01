c909f6fc3fcaea12f98b4c9d89c979fb4d05cf34
Emiliano <e.acevedo.felip@gmail.com>
2023-03-31 22:47:45 -0300

    set base path only in production

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const basePath = process.env.NODE_ENV === 'production' ? '/gamelab-catalog' : ''
+
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  basePath: '/gamelab-catalog',
+  basePath,
   images: { unoptimized: true }
 }
 

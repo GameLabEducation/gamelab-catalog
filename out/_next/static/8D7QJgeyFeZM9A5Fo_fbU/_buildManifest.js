47c33dab85b3b777bd850725ef7934be783450ee
Emiliano <e.acevedo.felip@gmail.com>
2023-03-31 23:10:31 -0300

    Deploy

--- out/_next/static/8D7QJgeyFeZM9A5Fo_fbU/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/959-34fb3915c9c5b84f.js","static/chunks/pages/index-320003abc5feb666.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

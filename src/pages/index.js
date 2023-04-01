c6b0d6a3d21b62490d81d0a1049f420259083e34
Emiliano <e.acevedo.felip@gmail.com>
2023-03-31 23:14:24 -0300

    favico href production ready

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,9 @@ import Header from '@/components/Header'
 import MainContent from '@/components/MainContent'
 import Head from 'next/head'
 import Footer from '@/components/Footer'
+const { basePath } = require('../../next.config')
+
+const iconHref = basePath + '/favicon.ico'
 
 export default function Home () {
   const [searchString, setSearchString] = useState('')
@@ -13,7 +16,7 @@ export default function Home () {
         <title>Gamelab Education catalog</title>
         <meta name='description' content='Games and Simulators catalog by Gamelab Education' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href={iconHref} />
       </Head>
       <Header setSearchString={setSearchString} />
       <MainContent searchString={searchString} />
